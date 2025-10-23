25cf1eb7a7fe86ed68bf4e04c531547be384b17f
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2025-10-23 12:39:30 -0300

    Add files via upload

---
+++ ATT-LUCAS-3TRI FORM/FORMJS.js
@@ -0,0 +1,19 @@
+const botaoAdicionar = document.querySelector('#adicionar');
+const formulario = document.querySelector('#pontos-form');
+const input = document.querySelector('#numero');
+
+botaoAdicionar.addEventListener('click',
+    function(event){
+    event.preventDefault();
+    },
+    console.log(pontos)
+);
+
+
+function Dados(formulario){
+
+   pontos = formulario.numero.value
+
+    return formulario,
+}
+ 
